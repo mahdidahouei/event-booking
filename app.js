65ecd6d6dda9a2f85e26101c26e0ad8e4704b963
Mahdi Arabpour <mahdi.a.200066@gmail.com>
2020-06-04 13:45:19 +0430

    setup a simple graphql

--- app.js
+++ app.js
@@ -1,9 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
+// ! sign makes the objects not nullable
+app.use('/graphql', graphQlHttp({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): String
+        }
+
+        schema{
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true,
+}));
 
 app.listen(PORT);
