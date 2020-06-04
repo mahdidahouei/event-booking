bee64a65136277518bb55216e6c2386869ebf313
Mahdi Arabpour <mahdi.a.200066@gmail.com>
2020-06-04 12:39:52 +0430

    setup server

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(PORT);
