84644d5f568d9e94bbc382523a2a2164c69d2e2f
Mahdi Arabpour <mahdi.a.200066@gmail.com>
2020-06-09 16:59:06 +0430

    allowed access to everybody

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use(isAuth);
 
 // ! sign makes the objects not nullable
